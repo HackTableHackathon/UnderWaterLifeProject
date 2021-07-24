6230880eee3239029ae18d59026d5d8a3a1458f7
nelso <nelso@DESKTOP-6FAA7A1.hsd1.wa.comcast.net>
2021-07-24 13:15:19 -0700

    testing

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends PApplet {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     PApplet.main("Main"); // do not add any other statement to your TreasureHunt.main() method
-
+    System.out.println("Steben lai");
   }
 
 }
