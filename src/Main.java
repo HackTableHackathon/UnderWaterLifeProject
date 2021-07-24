5088e4ed7b236dd431f4e41f9941ed2e147c1c5b
steve <steve@DESKTOP-RA526NF>
2021-07-24 12:06:54 -0700

    Added main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Main extends PApplet {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
